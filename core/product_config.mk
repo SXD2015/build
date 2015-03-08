38ef2aeb83693e2d40d9d039b623892b09b26772
David Vaz Guijarro <vazdavid@icloud.com>
2015-03-08 12:36:30 +0100

    Update product_config.mk

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,7 +185,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
   ifneq ($(AOSP_BUILD),)
-    all_product_configs := $(shell ls vendor/pa/products/aosp_$(AOSP_BUILD).mk)
+    all_product_configs := $(shell ls vendor/aosp/products/aosp_$(AOSP_BUILD).mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
